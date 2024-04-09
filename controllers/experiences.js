6d08e807f2255caf527b616ea08695a8fba5fa90
Vidhi Patel <vidhip99@gmail.com>
2024-04-09 07:27:30 -0400

    Implement Index functionality for experiences model

--- controllers/experiences.js
+++ controllers/experiences.js
@@ -1,38 +1,31 @@
-const Experience = require('../models/experience')
+const Experience = require('../models/experience');
 
-module.exports = {
-    new: newExperience,
-    create,
-    index
-};
-
-function newExperience(req, res){
-    res.render('experiences/new', { 
-        title: 'Create New Bucket List Item'
-    });
-}
-
-async function create(req, res){
+async function index(req, res) {
     try {
-        const experience = await Experience.create(req.body);
-        req.user.experiences.push(experience._id);
-        await req.user.save();
-        res.redirect('/experiences');
-    } catch(error){
-        console.log(error)
-        res.render('error', {title: 'Something went wrong'})
-    }
-}
-
-async function index (req, res){
-    try{
-        const user = await User.findById(req.user._id).populate('experiences');
-        res.render('experiences/index', {
-            experiences: user.experiences,
-            title: 'All Experiences'
-        });
+        const experiences = await Experience.find({});
+        res.render('experiences/index', { title: 'All Experiences', experiences });
     } catch (error) {
         console.log(error);
-        res.render('error', {title: 'Something Went Wrong'});
+        res.render('error', { message: "Failed to fetch experiences.", error });
     }
 }
+
+function newExperience(req, res) {
+    res.render('experiences/new', { title: 'Add Experience', errorMsg: '' });
+}
+
+async function create(req, res) {
+    try {
+        await Experience.create(req.body);
+        res.redirect('/experiences');
+    } catch (error) {
+        console.log(error);
+        res.render('error', { message: "Add experience failed.", error });
+    }
+}
+
+module.exports = {
+    index,
+    new: newExperience,
+    create
+};
